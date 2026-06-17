32e8ac6588e9e1c1d463eeede1ba01c564071f90
soumik3312 <soumikchaudhuri002@gmail.com>
2026-06-17 22:30:01 +0530

    video

--- src/utils/bunnySignedUrl.js
+++ src/utils/bunnySignedUrl.js
@@ -3,13 +3,22 @@ const crypto = require('crypto');
 function generateSignedUrl(videoId, libraryId) {
   if (!process.env.BUNNY_TOKEN_KEY) throw new Error('BUNNY_TOKEN_KEY is required');
   const expiryTime = Math.floor(Date.now() / 1000) + 3600;
+  const hostname = process.env.BUNNY_CDN_HOSTNAME;
+  const path = '/' + videoId + '/';
+  const hashableBase = process.env.BUNNY_TOKEN_KEY + path + expiryTime;
   const token = crypto
     .createHash('sha256')
-    .update(process.env.BUNNY_TOKEN_KEY + videoId + expiryTime)
-    .digest('hex');
+    .update(hashableBase)
+    .digest('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
+
+  const qs = `?token=${token}&expires=${expiryTime}&token_path=${encodeURIComponent(path)}`;
+
   return {
-    url480: `https://video.bunnycdn.com/play/${libraryId}/${videoId}/playlist.m3u8?token=${token}&expires=${expiryTime}&quality=480p`,
-    url720: `https://video.bunnycdn.com/play/${libraryId}/${videoId}/playlist.m3u8?token=${token}&expires=${expiryTime}&quality=720p`,
+    url480: `https://${hostname}/${videoId}/480p/video.m3u8${qs}`,
+    url720: `https://${hostname}/${videoId}/720p/video.m3u8${qs}`,
     expiresAt: expiryTime,
   };
 }
